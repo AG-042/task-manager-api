28ec2f69135974b437c0f24a64202b7e67b3bd1e
Nwobodo Chiagozie <nwobodochiagoziestanley@gmail.com>
2026-09-13 13:37:39 +0100

    Register project and task models in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Task
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "owner", "created_at")
+    search_fields = ("name", "owner__username", "owner__email")
+    filter_horizontal = ("members",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "project", "assignee", "status", "priority", "due_at")
+    list_filter = ("status", "priority")
+    search_fields = ("title", "project__name", "assignee__username")
